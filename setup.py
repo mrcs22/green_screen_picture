f4e55188054e751867579c66f25cf4d4fc373f0d
mrcs22 <marcusmoraes2010@hotmail.com>
2022-03-18 19:18:23 -0300

    fix: decode requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+with open("requirements.txt", "rb") as f:
+    encoded_requirements = f.read()
+    decoded_requirements = encoded_requirements.decode(
+        'utf-16le', 'ignore').splitlines()
+
+    requirements = [
+        req for req in decoded_requirements if req.find("==") >= 0]
+
 
 setup(
     name="green_screen_picture",
@@ -14,7 +20,7 @@ setup(
     description="simple package to add background images to green screen",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mrcs22/green_screen_picture"
+    url="https://github.com/mrcs22/green_screen_picture",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
